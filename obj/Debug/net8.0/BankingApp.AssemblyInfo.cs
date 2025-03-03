56c9a700f2e8b7492c7906782aae5be59f120eae
Sanduni-Abeysinghe <sanduniabeysinghe97@gmail.com>
2025-03-03 11:32:17 +0000

    db changes

--- obj/Debug/net8.0/BankingApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa879455c7b336e946d461762ae0d8995c53e47e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b5eb86b76c731fd4cfa512239bf2a798343feb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
